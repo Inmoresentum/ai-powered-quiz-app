10c8f19f094c2b1b612bf8d79800ee977d2bec81
Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>
2023-10-30 06:36:39 +0600

    chore: vaadin components are hard to customize, will use `shadcn` ui

--- frontend/views/helloworld/HelloWorldView.tsx
+++ frontend/views/helloworld/HelloWorldView.tsx
@@ -1,30 +1,37 @@
-import { Button } from '@hilla/react-components/Button.js';
-import { Notification } from '@hilla/react-components/Notification.js';
-import { TextField } from '@hilla/react-components/TextField.js';
-import { HelloWorldService } from 'Frontend/generated/endpoints.js';
-import { useState } from 'react';
+import {Button} from '@hilla/react-components/Button.js';
+import {Notification} from '@hilla/react-components/Notification.js';
+import {TextField} from '@hilla/react-components/TextField.js';
+import {HelloWorldService} from 'Frontend/generated/endpoints.js';
+import {useState} from 'react';
 
 export default function HelloWorldView() {
-  const [name, setName] = useState('');
+    const [name, setName] = useState('');
 
-  return (
-    <>
-      <section className="flex p-m gap-m items-end">
-        <TextField
-          label="Your name"
-          onValueChanged={(e) => {
-            setName(e.detail.value);
-          }}
-        />
-        <Button
-          onClick={async () => {
-            const serverResponse = await HelloWorldService.sayHello(name);
-            Notification.show(serverResponse);
-          }}
-        >
-          Say hello
-        </Button>
-      </section>
-    </>
-  );
+    return (
+        <>
+            <section className="flex items-baseline custom-stuff">
+                <TextField className="p-4"
+                           label="Your name"
+                           onValueChanged={(e) => {
+                               setName(e.detail.value);
+                           }}
+                />
+                <Button className="bg-green-500 text-white font-bold"
+                        onClick={async () => {
+                            const serverResponse = await HelloWorldService.sayHello(name);
+                            Notification.show(serverResponse);
+                        }}
+                >
+                    Say hello
+                </Button>
+            </section>
+
+            <TextField className="p-4"
+                       label="Your name"
+                       onValueChanged={(e) => {
+                           setName(e.detail.value);
+                       }}
+            />
+        </>
+    );
 }
