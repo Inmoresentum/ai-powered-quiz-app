89a72f0bb61b21a0b8cf781a053169e28e5a4f01
Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>
2023-10-30 07:35:08 +0600

    chore(deps): added shadcn ui and fixed the vite import error

--- frontend/views/helloworld/HelloWorldView.tsx
+++ frontend/views/helloworld/HelloWorldView.tsx
@@ -1,8 +1,8 @@
-import {Button} from '@hilla/react-components/Button.js';
 import {Notification} from '@hilla/react-components/Notification.js';
 import {TextField} from '@hilla/react-components/TextField.js';
 import {HelloWorldService} from 'Frontend/generated/endpoints.js';
 import {useState} from 'react';
+import {Button} from "@/components/ui/button";
 
 export default function HelloWorldView() {
     const [name, setName] = useState('');
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import react from '@vitejs/plugin-react';
 import type { UserConfigFn } from 'vite';
 import { overrideVaadinConfig } from './vite.generated';
+import path from "path";
 
 const customConfig: UserConfigFn = (env) => ({
   // Here you can add custom Vite parameters
@@ -10,6 +11,11 @@ const customConfig: UserConfigFn = (env) => ({
       include: '**/*.tsx',
     }),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "frontend/"),
+    },
+  },
 });
 
 export default overrideVaadinConfig(customConfig);
