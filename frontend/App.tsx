480f1219db7663456dd2455526e2e576d6ce3024
Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>
2023-10-30 06:34:50 +0600

    fix: fixed tailwind css not working properly

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,6 +1,7 @@
 import router from 'Frontend/routes.js';
 import { AuthProvider } from 'Frontend/util/auth.js';
 import { RouterProvider } from 'react-router-dom';
+import "./App.css"
 
 export default function App() {
   return (
