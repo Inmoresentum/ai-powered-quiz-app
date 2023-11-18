93411df03987afa57e08a86a01f70023cdb29ef8
Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>
2023-11-18 07:29:38 +0600

    chore(deps): added react query in the root App component

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -3,16 +3,19 @@ import {AuthProvider} from 'Frontend/util/auth.js';
 import {RouterProvider} from 'react-router-dom';
 import {HelmetProvider} from "react-helmet-async"
 import "./App.css"
+import {QueryClient, QueryClientProvider} from "react-query";
 
 const helmetContext = {};
-
+const queryClient = new QueryClient()
 export default function App() {
 
     return (
-        <HelmetProvider context={helmetContext}>
-            <AuthProvider>
-                <RouterProvider router={router}/>
-            </AuthProvider>
-        </HelmetProvider>
+        <QueryClientProvider client={queryClient}>
+            <HelmetProvider context={helmetContext}>
+                <AuthProvider>
+                    <RouterProvider router={router}/>
+                </AuthProvider>
+            </HelmetProvider>
+        </QueryClientProvider>
     );
 }
