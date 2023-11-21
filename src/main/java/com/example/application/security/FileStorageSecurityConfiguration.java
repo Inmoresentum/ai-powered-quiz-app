71ecb2b36b165b2248a17920a780d3f698295880
Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>
2023-11-21 11:17:13 +0600

    fix: added a separate security configuration for FileStorageController
    It's simply because it will not be heralded by hilla, in fact it should not be handled by hilla at least as of now unless vaadin figures out how to deal with form data

---
+++ src/main/java/com/example/application/security/FileStorageSecurityConfiguration.java
@@ -0,0 +1,29 @@
+package com.example.application.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class FileStorageSecurityConfiguration {
+
+    @Bean
+    @Order(1)
+    SecurityFilterChain fileStorageConfig(HttpSecurity http) throws Exception {
+        return http
+                .securityMatcher(AntPathRequestMatcher.antMatcher("/api/**"))
+                .authorizeHttpRequests( auth -> {
+                    auth.requestMatchers(AntPathRequestMatcher.antMatcher("/api/**")).permitAll();
+                })
+                .csrf(csrf -> csrf.ignoringRequestMatchers(AntPathRequestMatcher.antMatcher("/api/**")))
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+                .build();
+
+    }
+}
