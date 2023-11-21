88d1b1c38ee161211387473a96cdf783f039e423
Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>
2023-11-21 08:51:50 +0600

    feat: added the clamAV service for file scanning

---
+++ src/main/java/com/example/application/service/clamAV/ClamAVService.java
@@ -0,0 +1,36 @@
+package com.example.application.service.clamAV;
+
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Service
+public class ClamAVService {
+    private final WebClient webClient;
+
+    public ClamAVService(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder.baseUrl("http://localhost:7075").build();
+    }
+
+    public boolean scanFile(Resource fileData) {
+        try {
+            String result = webClient.post()
+                    .uri("/scan")
+                    .contentType(MediaType.MULTIPART_FORM_DATA)
+                    .body(BodyInserters.fromMultipartData("file", fileData))
+                    .retrieve()
+                    .bodyToMono(String.class)
+                    .block();
+
+            System.out.println(result);
+            System.out.println(result != null && result.contains("OK"));
+            return result != null && result.contains("OK");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+
+    }
+}
