6e6d6bf4708a5327bfe72fda442448ad75701aec
Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>
2023-11-25 08:36:46 +0600

    chore: cleaned up the MainLayout.tsx

--- frontend/views/MainLayout.tsx
+++ frontend/views/MainLayout.tsx
@@ -1,16 +1,10 @@
 import Placeholder from 'Frontend/components/placeholder/Placeholder.js';
-import {useRouteMetadata} from 'Frontend/util/routing.js';
 import {Suspense} from 'react';
 import {Outlet} from 'react-router-dom';
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
 
-const navLinkClasses = ({isActive}: any) => {
-    return `block rounded-m p-s ${isActive ? 'bg-primary-10 text-primary' : 'text-body'}`;
-};
-
 export default function MainLayout() {
-    const currentTitle = useRouteMetadata()?.title ?? 'My App';
     return (
         <>
             <div className="flex flex-col justify-between min-h-screen">
