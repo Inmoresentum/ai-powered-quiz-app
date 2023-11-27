d3efa922df5bcbeb133c6ae9e783f48f6f3aadad
Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>
2023-11-27 06:10:21 +0600

    feat: added SEO or search engine optimizations for home page

--- frontend/views/HomePage/HomePage.tsx
+++ frontend/views/HomePage/HomePage.tsx
@@ -1,8 +1,15 @@
 import Hero from "@/components/landingpage/hero";
+import {Helmet} from "react-helmet-async";
 
 export default function HomePage() {
     return (
         <>
+                <Helmet>
+                    <title>
+                        Home
+                    </title>
+                    <meta name="description" content="Welcome to QuizBotIQ"/>
+                </Helmet>
                 <div
                     className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-rose-100 to-teal-100 rounded-full md:rounded-t-full md:rounded-b-[1480px] md:mr-40 md:ml-40 backdrop-blur">
                     <Hero/>
