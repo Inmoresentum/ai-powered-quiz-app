4761a01c407383f0634f92ebf0a798ada89cfc9c
Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>
2023-12-05 00:55:46 +0600

    feat: added react-confetti library for enhanced visual experience
    Added the react-confetti library to the project and used it in the PaymentSuccess component. This library will be used to generate a confetti animation whenever a payment is successfully processed. This will enhance the visual appeal of the application and provide a more positive feedback to the user upon successful transactions.

--- frontend/views/payment/success.tsx
+++ frontend/views/payment/success.tsx
@@ -2,6 +2,7 @@ import {Helmet} from "react-helmet-async";
 import {Link, NavLink} from "react-router-dom";
 import {Separator} from "@/components/ui/separator";
 import React from "react";
+import ReactConfetti from "react-confetti";
 
 export default function PaymentSuccess() {
     return (
@@ -12,7 +13,7 @@ export default function PaymentSuccess() {
                 <meta name="description"
                       content="This is componenet is for displaying message for payment xD"/>
             </Helmet>
-
+            <ReactConfetti gravity={0.1}/>
             <div
                 className="bg-gray-100 min-w-full rounded-2xl md:min-w-[450px] lg:md:min-w-[550px] flex flex-col shadow-xl items-center justify-center hover:shadow-2xl drop-shadow-2xl duration-300 ease-linear hover:bg-gray-100 md:m-20 md:hover:scale-105">
                 <Link to="/" className="flex flex-row items-center justify-center">
