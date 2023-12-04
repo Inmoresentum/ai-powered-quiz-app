539ad0906642466b54b33634aa29b54c366efdf8
Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>
2023-12-05 01:10:37 +0600

    feat: add cookie consent component for user data transparency
    Added a new CookieConsent component and integrated it in MainLayout. This change also includes adding "react-cookie-consent" to our package dependencies. This facilitates user consent for using cookies to enhance their experience on our website. Also, with GDPR and another privacy legislation, it is crucial to transparently inform users about our use of their data.

---
+++ frontend/components/CookieConsent.tsx
@@ -0,0 +1,45 @@
+import {CookieConsent} from "react-cookie-consent";
+import React from "react";
+import {Link} from "react-router-dom";
+import {Cookie} from "lucide-react";
+
+export default function CookieConsentWrapper() {
+    return (
+        <CookieConsent
+            location="bottom"
+            cookieName="myAwesomeCookieName3"
+            expires={999}
+            overlay={true}
+            buttonText={
+                <div className="flex items-center ">
+                    <Cookie className="mr-2" size={55}/>
+                    Yum Cookies Are Delicious
+                </div>
+            }
+            buttonClasses="bg-gradient-to-r from-green-500 to-blue-600 text-white
+             font-bold py-2 px-4 rounded-xl transition duration-500 ease-linear
+              hover:from-purple-400 hover:to-indigo-500 hover:text-black
+               shadow-black hover:shadow-2xl"
+            containerClasses="bg-white
+             p-4 flex justify-between items-center fixed
+             bottom-0 left-0 right-0 z-50 max-w-screen-lg mx-auto rounded-3xl shadow-2xl"
+            contentClasses="text-black text-lg"
+            disableStyles={true}
+        >
+            <div className="flex items-center">
+                {/*<MdClose className="h-5 w-5 text-gray-500 mr-2" />*/}
+                <span className="text-black font-medium">
+        <strong>Welcome to Our Website!</strong> This website uses cookies to enhance the user experience.
+                    We want to ensure that you have control over your data and understand how we use cookies and similar technologies.
+                    By continuing to browse and use this website, you consent to the use of cookies as described in this Cookie Consent
+                    in <Link to={"privacy-policy"}
+                             className="text-orange-700 hover:text-green-600 hover:font-semibold
+                              hover:scale-105 duration-300 ease-linear hover:underline
+                              focus:outline-none focus:ring-2 focus:ring-blue-500 rounded">
+                              Privacy Policy
+                        </Link> so that we can keep track of you and sell your data 🍪.
+                </span>
+            </div>
+        </CookieConsent>
+    );
+}
--- frontend/views/MainLayout.tsx
+++ frontend/views/MainLayout.tsx
@@ -3,11 +3,13 @@ import {Suspense} from 'react';
 import {Outlet} from 'react-router-dom';
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
+import CookieConsentWrapper from "@/components/CookieConsent";
 
 export default function MainLayout() {
     return (
         <>
             <div className="flex flex-col justify-between min-h-screen">
+                <CookieConsentWrapper/>
                 <Navbar/>
                 <Suspense fallback={<Placeholder/>}>
                     <Outlet/>
