56396813914a3eda97465ba52d23f5e82596e39e
Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>
2023-12-11 23:02:42 +0600

    feat: adjusted some minor styles

--- frontend/views/quiz/play/quizPlayer.tsx
+++ frontend/views/quiz/play/quizPlayer.tsx
@@ -1,16 +1,19 @@
 import Quiz from "@/components/quiz-components/core-starter/Quiz";
 import {quiz} from "./QuizMockData"
+
 export default function QuizPlayer() {
     return (
-        <Quiz
-            quiz={quiz}
-            shuffle={true}
-            // showInstantFeedback
-            // continueTillCorrect
-            onQuestionSubmit={(obj: any) => console.log("user question results:", obj)}
-            disableSynopsis={false}
-            // revealAnswerOnSubmit={true}
-            // allowNavigation={true}
-        />
+        <div className="flex items-center justify-center bg-gradient-to-r from-rose-100 to-teal-100 rounded-3xl">
+            <Quiz
+                quiz={quiz}
+                shuffle={true}
+                // showInstantFeedback
+                // continueTillCorrect
+                onQuestionSubmit={(obj: any) => console.log("user question results:", obj)}
+                disableSynopsis={false}
+                // revealAnswerOnSubmit={true}
+                // allowNavigation={true}
+            />
+        </div>
     );
 }
