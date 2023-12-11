7bb447852ddb436c1816ed89892c0412e6c23d4b
Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>
2023-12-11 22:55:34 +0600

    feat(frontend): added a fully functional Quiz views

--- frontend/views/quiz/play/quizPlayer.tsx
+++ frontend/views/quiz/play/quizPlayer.tsx
@@ -1,7 +1,16 @@
+import Quiz from "@/components/quiz-components/core-starter/Quiz";
+import {quiz} from "./QuizMockData"
 export default function QuizPlayer() {
     return (
-        <div className="text-red-500 text-7xl">
-            Not Implemented
-        </div>
+        <Quiz
+            quiz={quiz}
+            shuffle={true}
+            // showInstantFeedback
+            // continueTillCorrect
+            onQuestionSubmit={(obj: any) => console.log("user question results:", obj)}
+            disableSynopsis={false}
+            // revealAnswerOnSubmit={true}
+            // allowNavigation={true}
+        />
     );
 }
