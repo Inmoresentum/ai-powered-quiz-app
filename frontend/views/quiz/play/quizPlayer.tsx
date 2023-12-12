0bf133ee5a9e57511830bcaeb43832b42deb5e18
Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>
2023-12-12 07:19:42 +0600

    fix: replaced hard-coded quiz data with API call in QuizPlayer
    Updated the QuizPlayer component to use the react-query library for fetching quiz data based on the id from the route parameters. Replaced the static quiz mock data with API call to enable dynamic loading of quizzes in QuizPlayer component.

--- frontend/views/quiz/play/quizPlayer.tsx
+++ frontend/views/quiz/play/quizPlayer.tsx
@@ -1,12 +1,18 @@
 import Quiz from "@/components/quiz-components/core-starter/Quiz";
-import {quiz} from "./QuizMockData"
+import {useParams} from "react-router-dom";
+import {useQuery} from "react-query";
+import {QuizEndpoint} from "@/generated/endpoints";
 
 export default function QuizPlayer() {
-
+    const param = useParams();
+    const quizId = parseInt(param.id as string);
+    const query = useQuery({queryKey: ['quiz', param.id], queryFn: () => QuizEndpoint.getQuiz(quizId)})
+    if (query.status === "loading") return <div>Loading...</div>
+    console.log(query.data);
     return (
         <div className="flex items-center justify-center bg-gradient-to-r from-rose-100 to-teal-100 rounded-3xl">
             <Quiz
-                quiz={quiz}
+                quiz={query.data}
                 shuffle={true}
                 // showInstantFeedback
                 // continueTillCorrect
